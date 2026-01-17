159fe66a2504e63ec3f391358371ba3f78ce399d
govindakandel <govindkandel09@gmail.com>
2026-01-17 13:52:24 +0545

    singn and login api expose

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,3 +3,5 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const PORT = process.env.PORT
+export const MONGO_URI = process.env.MONGO_URI
+export const JWT_KEY = process.env.JWT_KEY
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import app from "./app.js"
 import { PORT } from "./config/env.js"
+import { connectDB } from "./config/db.js"
 
 
+connectDB() ;
 
 app.listen(PORT , ()=> {
     console.log(`Server is running on ${PORT}`)
