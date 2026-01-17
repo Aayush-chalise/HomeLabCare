b8c8eced7cab181c5096f928b67afa59d41de063
govindakandel <govindkandel09@gmail.com>
2026-01-17 14:55:28 +0545

    auth added

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,7 +1,6 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+dotenv.config();
 
-dotenv.config()
-
-export const PORT = process.env.PORT
-export const MONGO_URI = process.env.MONGO_URI
-export const JWT_KEY = process.env.JWT_KEY
+export const PORT = process.env.PORT 
+export const MONGO_URI = process.env.MONGO_URI 
+export const JWT_SECRET = process.env.JWT_SECRET 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
-import app from "./app.js"
-import { PORT } from "./config/env.js"
-import { connectDB } from "./config/db.js"
+import app from "./app.js";
+import { PORT } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 
-connectDB() ;
-
-app.listen(PORT , ()=> {
-    console.log(`Server is running on ${PORT}`)
-})
